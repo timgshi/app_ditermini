6167bd4c74659ba7d97aef514e6e4ab2ffce1ad7
Dhruv Amin <dhruv92@stanford.edu>
2012-11-06 22:58:31 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppDitermini::Application.config.secret_token = '0ad275a5003b17b6a7fa47981575aa390b3908947ae65c21744d6d7aad4c87b5df61c60965a3dee93feff221db45303fdd581097d19a10bf35c6d8fb57d51385'
